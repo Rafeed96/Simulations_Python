374ffe52ce35fffb861123b64a89d5c5bbdb4f81
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-05 23:34:40 +0600

    Update Area.py

---
+++ Area.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(1)
+
+x_min = 0
+y_min = 0
+
+x_max = 2
+y_max = 1
+
+n = 1000000
+
+hit = 0
+
+hit_X = []
+hit_Y = []
+
+miss_X = []
+miss_Y = []

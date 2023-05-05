5110e026c9d84f01a47c28d49df5b6571240148d
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-05 20:57:58 +0600

    Update Buffon's_Needle_experiment.py

---
+++ Buffon's_Needle_experiment.py
@@ -0,0 +1,12 @@
+# Gap = d = 1
+# Needle = l = 1 , (l<=d)!!!
+# Distance of middle part of needle to nearest line = D = (0,d/2)
+# 0 = Theta = 0 => (0, pi)
+# h = l/2*sin0
+# sin0 = h/(l/2)
+
+# Hit Conditions
+# h >= D
+# D <= (l/2)sin0
+
+# pi = (2*n)/hits

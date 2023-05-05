36de12cd54fd4863819000889793ab68f0534a04
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-05 23:30:21 +0600

    Update Monte_Carlo.py

--- Monte_Carlo.py
+++ Monte_Carlo.py
@@ -16,3 +16,13 @@ x_max = 5
 
 b = 2
 a = 0
+
+
+for i in range(n):
+    x = np.random.uniform(low=a, high=b)
+
+    y = math.exp(x)  # OR np.exp(x)
+
+    f_sum = f_sum + y
+
+    f_sq_sum = f_sq_sum + y**2

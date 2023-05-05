452d553dfc2a9ab8357c008b897c43c45957c1a3
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-05 20:47:05 +0600

    Create Python_Basics.py

---
+++ Day 1 Tasks/Python_Basics.py
@@ -0,0 +1,17 @@
+num = 5
+print(num)
+type(num)
+num = 'this is a string'
+print(num,type(num))
+
+
+#input
+
+a = input()
+# type  cast
+a = float(a)
+a = a + 10
+print("Given Input: ",a)
+
+a += 10  
+print(a)

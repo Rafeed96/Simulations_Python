62c2b56a57d60136ecb2550e6cca9a0d5a6a19c7
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-06 22:36:30 +0600

    Create RNG_Mid_Square_Method.py

---
+++ RNG_Mid_Square_Method.py
@@ -0,0 +1,19 @@
+Z = [71823456]
+U = []
+
+while True:
+
+    tmp = Z[-1]
+    tmp_square = tmp**2
+    last_six = tmp_square % 10000000000
+    mid_four = int(last_six / 10000)
+
+    if mid_four in Z:
+        break
+    else:
+        Z.append(mid_four)
+        U.append(mid_four/10000)
+
+    print(U)
+    print(Z)
+    print(len(Z))

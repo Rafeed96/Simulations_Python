77aa507f4e10c74db09dc9c5fad820d184fd3f7f
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-06 22:24:59 +0600

    Update Pure_Pursuit.py

--- Pure_Pursuit.py
+++ Pure_Pursuit.py
@@ -26,3 +26,26 @@ P_y = [100]
 
 for t in range(20):
     dist = math.sqrt((T_x[t]-P_x[t])**2 + (T_y[t]-P_y[t])**2)
+
+    if dist < 10:
+        print("Target Destroyed !!")
+
+    CosTheta = (T_x[t] - P_x[t]) / dist
+    SineTheta = (T_y[t] - P_y[t]) / dist
+
+    x_new = P_x[t] + S * CosTheta
+    y_new = P_y[t] + S * SineTheta
+
+    P_x.append(x_new)
+    P_y.append(y_new)
+
+print(len(P_x))
+
+plt.scatter(T_x, T_y)
+plt.plot(T_x, T_y)
+
+plt.scatter(P_x, P_y)
+plt.plot(P_x, P_y)
+
+
+plt.show()

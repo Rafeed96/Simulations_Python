6bdbd17fba8fbd78804382261f0bd9f5793e59ce
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-08 21:37:35 +0600

    Update Inventory_System.py

--- Inventory_System.py
+++ Inventory_System.py
@@ -17,3 +17,16 @@ shortage_day_count = 0
 avg_ending = 0
 end_Graph = []
 days = []
+
+
+for cycle in range(10):
+    print("Cycle no: ", cycle)
+    for day in range(1, N+1):
+        print("Day no: ", day)
+
+        days_until_arrival = days_until_arrival - 1
+
+        # calculate beginning_inventory, consider arrival of orders placed
+        if days_until_arrival < 0:
+            beginning_inventory = beginning_inventory + order_quantity
+            order_quantity = 0

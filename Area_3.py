65a8b218ed63505a78a8cf8256325ba706a075a7
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-08 22:14:06 +0600

    Update Area_3.py

---
+++ Area_3.py
@@ -0,0 +1,21 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(1)
+
+x_min = 0
+y_min = 0
+
+x_max = 3
+y_max = 2
+
+n = 1000000
+
+hit = 0
+
+hit_X = []
+hit_Y = []
+
+miss_X = []
+miss_Y = []

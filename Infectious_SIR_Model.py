5998bae9ae25c05a204088aa5860baf01a275a28
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-17 21:24:49 +0600

    Update Infectious_SIR_Model.py

--- Infectious_SIR_Model.py
+++ Infectious_SIR_Model.py
@@ -3,3 +3,12 @@
 import scipy.integrate
 import numpy
 import matplotlib.pyplot as plt
+
+
+def SIR_model(y, t, beta , gamma):
+  S , I , R = y
+  dS_dt = -beta*S*I
+  dI_dt = beta*S*I - gamma*I
+  dR_dt = gamma*I
+
+  return([dS_dt, dI_dt, dR_dt,])

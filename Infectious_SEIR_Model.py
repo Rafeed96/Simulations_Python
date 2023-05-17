1cf136af0f5d68546311209778a593ae7500ae5f
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-17 21:52:44 +0600

    Update Infectious_SEIR_Model.py

--- Infectious_SEIR_Model.py
+++ Infectious_SEIR_Model.py
@@ -3,3 +3,15 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+
+
+# 0 = No social distancing
+# 0.1 = Masks
+# 0.2 = Masks and hybrid classes
+# 0.3 = Masks and hybrid classes and online classes
+
+u = 0.1   # Social Distancing Rate = (0-1)
+t_incubation = 5.1
+t_infective = 3.3
+R0 = 2.4  # 
+N = 33517 # student population

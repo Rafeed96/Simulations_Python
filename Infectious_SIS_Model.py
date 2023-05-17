441a72020595f078d4810246a735de8f50e76ab6
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-17 21:10:55 +0600

    Update Infectious_SIS_Model.py

---
+++ Infectious_SIS_Model.py
@@ -0,0 +1,8 @@
+import matplotlib.pylab as plt
+
+N = 10000  # Total population
+S = N - 1  # Initial susceptible Population
+I = 1  # Initial infected population
+beta = 0.2  # Describes the infection rate of the disease.
+gamma = 0.04  # Describes recovery rate.
+T = 365  # Number Of Days

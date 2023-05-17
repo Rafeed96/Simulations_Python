8281c49841b51b4f064ecc11290d40375337893c
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-17 21:11:16 +0600

    Update Infectious_SIS_Model.py

--- Infectious_SIS_Model.py
+++ Infectious_SIS_Model.py
@@ -6,3 +6,16 @@ I = 1  # Initial infected population
 beta = 0.2  # Describes the infection rate of the disease.
 gamma = 0.04  # Describes recovery rate.
 T = 365  # Number Of Days
+
+
+
+#Compartments
+susceptible = [] 
+infected = []
+
+def funcSIS(S, I, N, T):
+    for t in range (0, T):
+        S,I = S - (beta*S*I/N) + gamma * I , I + (beta*S*I/N) - gamma * I
+
+        susceptible.append(S)
+        infected.append(I)

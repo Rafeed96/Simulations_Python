c044ca3ee61824e1b93a06e1463eea8b11411080
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-17 21:54:24 +0600

    Update Infectious_SEIR_Model.py

--- Infectious_SEIR_Model.py
+++ Infectious_SEIR_Model.py
@@ -13,8 +13,8 @@ import matplotlib.pyplot as plt
 u = 0.1   # Social Distancing Rate = (0-1)
 t_incubation = 5.1
 t_infective = 3.3
-R0 = 2.4  # 
-N = 33517 # student population
+R0 = 2.4  #
+N = 33517  # student population
 
 
 # initial number of infected and recovered individual
@@ -22,8 +22,26 @@ e0 = 1/N
 i0 = 0.00
 r0 = 0.00
 s0 = 1 - e0 - i0 - r0
-x0 = [s0,e0,i0,r0]
+x0 = [s0, e0, i0, r0]
 
 alpha = 1/t_incubation
 gamma = 1/t_infective
 beta = R0*gamma
+
+
+def covid(x, t):
+    s, e, i, r = x
+    dx = np.zeros(4)
+    dx[0] = -(1-u)*beta * s * i
+    dx[1] = (1-u)*beta * s * i - alpha * e
+    dx[2] = alpha * e - gamma * i
+    dx[3] = gamma * i
+    return dx
+
+
+t = np.linspace(0, 200, 101)
+x = odeint(covid, x0, t)
+s = x[:, 0]
+e = x[:, 1]
+i = x[:, 2]
+r = x[:, 3]

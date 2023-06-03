0fba10274d8636ac9e960ebf2730f762e521d1c6
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-03 06:21:53 +0600

    Create Planets_Simulations.py

---
+++ Planets_Simulations.py
@@ -0,0 +1,12 @@
+# Install Pygame package
+
+import pygame
+import math
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("Planet Simulation")
+

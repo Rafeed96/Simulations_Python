5d693ad874bc97f8891399f677fb006a7fc5e31c
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-05 21:34:28 +0600

    Update Planets_Simulations.py

--- Planets_Simulations.py
+++ Planets_Simulations.py
@@ -13,7 +13,8 @@ pygame.display.set_caption("Planet Simulation")
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 BLUE = (100, 149 , 237)
-
+RED = (188, 39 , 50)
+DARK_GREY = (100, 100, 100)
 
 class Planet:
     AU = 149.6e6 * 1000  # Astonimical Units
@@ -50,7 +51,11 @@ def main():
 
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
 
-    planets = [sun, earth]
+    mars = Planet(-1.524 * Planet.AU, 0 ,12, RED, 6.39 * 10**23)
+
+    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**24)
+
+    planets = [sun, earth, mars, mercury]
 
     while run:
         clock.tick(60)

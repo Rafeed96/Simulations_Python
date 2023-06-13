aded497c525f615c1000d54dc55d9833f4364588
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-13 21:22:11 +0600

    Update Planet_Animation.py

--- Planet_Animation.py
+++ Planet_Animation.py
@@ -2,3 +2,16 @@ import turtle
 import time
 
 wn = turtle.Screen()
+wn.title("Solar System")
+wn.bgpic("https://images.unsplash.com/photo-1544656376-ffe19d4b7353?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmxhY2slMjBza3l8ZW58MHx8MHx8fDA%3D&w=1000&q=80")
+wn.setup(width=1750, height=1000)
+
+pen = turtle.Turtle()
+pen.ht()
+pen.pu()
+
+sun = turtle.Turtle()
+sun.shape('circle')
+sun.shapesize(stretch_len=8, stretch_wid=8)
+sun.color('yellow')
+sun.pu()
